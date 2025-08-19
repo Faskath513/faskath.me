b2f6dac97f9183a937521fb11ed7cf2686dd2b42
Faskath513 <faskath9054@gmail.com>
2025-08-19 13:13:15 +0530

    Image set mobile view

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -339,7 +339,7 @@ const HeroSection = () => {
           {/* Scroll Indicator */}
           <motion.div
             variants={itemVariants}
-            className="absolute left-1/2 transform -translate-x-1/2"
+            className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
         >
           <motion.div
             animate={{ y: [0, 10, 0] }}
